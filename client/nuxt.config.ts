c8bba1b10d68be6086dc0921e4ec6083cd07e64c
Krista Simmons <kristasimmons.design@gmail.com>
2023-03-25 09:29:48 -0700

    Vuetify configuration

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -1,4 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { resolve } from "path"
 export default defineNuxtConfig({
-
+  alias: {
+    "@": resolve(__dirname, "./client"),
+  },
+  css: [
+    'vuetify/lib/styles/main.sass', 
+    '@mdi/font/css/materialdesignicons.min.css'
+  ],
+  build: {
+    transpile: ['vuetify'],
+  },
+  vite: {
+    define: {
+      'process.env.DEBUG': false,
+    },
+  }
 })
