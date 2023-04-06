1015a6f0e51d291c4636fb1e5372ffed068955d7
Krista Simmons <kristasimmons.design@gmail.com>
2023-04-05 20:09:19 -0700

    Adds action to fetch single trip

--- client/components/Trip/TripDetail.vue
+++ client/components/Trip/TripDetail.vue
@@ -1,13 +1,15 @@
 <template>
   <div>
     <h1>Trip Detail Page</h1>
+    {{ trip }}
   </div>
 </template>
 
 <script lang="ts">
 /*
-    Props
+    Imports
 */
+import { useStoreTrips } from "../../store/storeTrips"
 
 export default defineComponent({
   props: {
@@ -15,6 +17,20 @@ export default defineComponent({
       type: Object,
       required: true,
     },
+  },
+  setup() {
+    /*
+        Store
+    */
+    const storeTrips = useStoreTrips()
+
+    let tripData = storeTrips.fetchTrip(`road-trip-pacific-coast-highway`)
+
+    console.log(tripData)
+
+    return {
+      trip: storeTrips.trip,
+    }
   },
 })
 </script>
--- client/store/storeTrips.ts
+++ client/store/storeTrips.ts
@@ -14,18 +14,26 @@ interface Trip {
 
 export const useStoreTrips = defineStore({
   id: "trips",
-  state: (): { tripsList: Trip[] } => ({
+  state: (): { tripsList: Trip[]; trip: Trip | null } => ({
     tripsList: [],
+    trip: null,
   }),
   actions: {
     async fetchTrips() {
       const response = await fetch("http://localhost:8000/api/trips")
       this.tripsList = await response.json()
     },
+    async fetchTrip(slug: string) {
+      const response = await fetch("http://localhost:8000/api/trips/${slug}")
+      this.trip = await response.json()
+    },
   },
   getters: {
     tripsArray(): Trip[] {
       return this.tripsList
+    },
+    currentTrip(): Trip | null {
+      return this.trip
     },
   },
 })
